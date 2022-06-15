ce2c42659666b3ee1701c0f65eb6717ffc3c82e1
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2022-06-15 18:07:54 +0800

    Update run.sh

--- fqkk/run.sh
+++ fqkk/run.sh
@@ -14,12 +14,13 @@ arou=$ou
 fi
 }
 fqrw(){
-curl -s -k -i --raw -o fqrw "http://$rk/" -H "Host: $rk" -H "Proxy-Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-Encoding: " -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
-rw=$(cat fqrw | grep -o "http.*" | cut -d '/' -f3)
+url=$(curl -Ls "http://m.oo2u4.cn/entry" | grep -o "url_h5 =.*" | grep -o "：.*" | grep -o "[a-z0-9:./-]*")
+rk=$(curl -s "$url" | grep -o "get('.*" | cut -d "'" -f2)
+rw=$(curl -s "$rk" -H "User-Agent:$ua" | grep -o "http.*" | cut -d '/' -f3 | cut -d '\' -f1)
 }
 fqzlm(){
 if [ ! -z $zlm ]
-then zlg=$(curl -s "http://$rw/zs?for=$zlm" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Cookie: $ck" -H "Accept-Encoding: " -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7" | grep -o 'background)".*' | cut -d '>' -f2 | cut -d '<' -f1)
+then zlg=$(curl -s "http://$rw/zs?for=$zlm" -H "User-Agent:$ua" -H "Cookie:$ck" | grep -o 'background)".*' | cut -d '>' -f2 | cut -d '<' -f1)
 if [ ! -z $zlg ]
 then zlg=为$zlg助力，
 else dt=$(date '+%Y-%m-%d %H:%M:%S')
@@ -28,7 +29,7 @@ fi
 fi
 }
 fqfr(){
-curl -s -k -i --raw -o fqfr "http://$rw/fast_reada" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-Encoding: " -H "Cookie: $ck" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+curl -s -k -i --raw -o fqfr "http://$rw/fast_reada" -H "User-Agent:$ua" -H "Cookie:$ck"
 if [ $(grep -c 'userinfo-score"' fqfr) -eq 1 ]
 then b=$(cat fqfr | grep -o 'userinfo-score".*' | cut -d ':' -f2 | cut -d '<' -f1)
 [ $(echo $b | grep -c ".") -eq 1 ] && b=$(echo $b | cut -d '.' -f1)
@@ -43,7 +44,7 @@ fi
 }
 fqrft(){
 if [ ! -s ckerror ]
-then curl -s -k -i --raw -o fqrft -X POST -d "readLastKey=" "http://$rw/reada/finishTask" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "Accept: */*" -H "Origin: http://$rw" -H "X-Requested-With: XMLHttpRequest" -H "User-Agent: $ua" -H "Content-Type: application/x-www-form-urlencoded" -H "Cookie: $ck" -H "Referer: http://$rw/fast_reada" -H "Accept-Encoding: " -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+then curl -s -k -i --raw -o fqrft -X POST -d "readLastKey=" "http://$rw/reada/finishTask" -H "X-Requested-With: XMLHttpRequest" -H "User-Agent:$ua" -H "Cookie:$ck"
 if [ $(grep -c "data" fqrft) -eq 1 ]
 then n=$(cat fqrft | grep -o "num.*" | cut -d ',' -f1 | cut -d '"' -f3)
 [ -z $n ] && n=0
@@ -72,7 +73,7 @@ fi
 fqfrdr(){
 [ ! -z $zlm ] && zs=1
 r=$(awk 'BEGIN{srand();printf "%.16f\n",rand()}')
-curl -s -k -i --raw -o fqfrdr "http://$rw/fast_reada/do_read?for=$zlm&zs=$zs&pageshow&r=$r&jkey=$jkey" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "User-Agent: $ua" -H "X-Requested-With: XMLHttpRequest" -H "Accept: */*" -H "Cookie: $ck" -H "Referer: http://$rw/fast_reada/read" -H "Accept-Encoding: " -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+curl -s -k -i --raw -o fqfrdr "http://$rw/fast_reada/do_read?for=$zlm&zs=$zs&pageshow&r=$r&jkey=$jkey" -H "User-Agent:$ua" -H "X-Requested-With: XMLHttpRequest" -H "Cookie:$ck"
 if [ $(grep -c '"jkey"' fqfrdr) -eq 1 -a $(grep -c "oauth2" fqfrdr) -eq 1 ]
 then jkey=$(cat fqfrdr | grep -o "jkey.*" | cut -d '"' -f3)
 ja=$(cat fqfrdr | grep -o "appid.*" | cut -d '&' -f1 | cut -d '=' -f2)
@@ -84,22 +85,22 @@ fi
 drm=$(cat fqfrdr | grep -o "msg.*" | cut -d '"' -f3)
 }
 owa(){
-uuid=$(curl -s -H "Host: open.weixin.qq.com" -H "upgrade-insecure-requests: 1" -H "user-agent: $ua" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "sec-fetch-user: ?1" -H "sec-fetch-dest: document" -H "accept-language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7" --compressed "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ja&redirect_uri=http%3A%2F%2F$ju%2Ffast_reada%2Foiejr%3Fjumpid%3D$ji&response_type=code&scope=snsapi_userinfo&state=$js&connect_redirect=1&uin=$aou&key=$key&version=$ov&pass_ticket=$op" | grep -o "&uuid.*" | cut -d '&' -f2 | cut -d '=' -f2)
+uuid=$(curl -s -H "user-agent:$ua" --compressed "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ja&redirect_uri=http%3A%2F%2F$ju%2Ffast_reada%2Foiejr%3Fjumpid%3D$ji&response_type=code&scope=snsapi_userinfo&state=$js&connect_redirect=1&uin=$aou&key=$key&version=$ov&pass_ticket=$op" | grep -o "&uuid.*" | cut -d '&' -f2 | cut -d '=' -f2)
 if [ -z $uuid ]
 then dt=$(date '+%Y-%m-%d %H:%M:%S')
 echo $dt owsq常量失效，请重新运行脚本并输入新的owsq > ckerror
 fi
 }
 owar(){
-curl -s -k -i --raw -o owar "https://open.weixin.qq.com/connect/oauth2/authorize_reply?allow=1&snsapi_userinfo=on&uuid=$uuid&uin=$ou&key=$key&pass_ticket=$op&version=$ov" -H "Host: open.weixin.qq.com" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Dest: document" -H "Referer: https://open.weixin.qq.com/connect/oauth2/authorize?appid=$ja&redirect_uri=http%%3A%%2F%%2F$ju%%2Ffast_reada%%2Foiejr%%3Fjumpid%%3D$ji&response_type=code&scope=snsapi_userinfo&state=$js&connect_redirect=1&uin=$arou&key=$key&version=$ov&pass_ticket=$op" -H "Accept-Encoding: gunzip, br" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+curl -s -k -i --raw -o owar "https://open.weixin.qq.com/connect/oauth2/authorize_reply?allow=1&snsapi_userinfo=on&uuid=$uuid&uin=$ou&key=$key&pass_ticket=$op&version=$ov" -H "User-Agent:$ua"
 jc=$(cat owar | grep -o "code.*" | cut -d '&' -f1 | cut -d '=' -f2)
 }
 joj(){
-curl -s -k -i --raw -o joj "http://$ju/fast_reada/oiejr?jumpid=$ji&code=$jc&state=$js" -H "Host: $ju" -H "Proxy-Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Dest: document" -H "Accept-Encoding: " -H "Cookie: $ckP; $cku" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+curl -s -k -i --raw -o joj "http://$ju/fast_reada/oiejr?jumpid=$ji&code=$jc&state=$js" -H "User-Agent:$ua" -H "Cookie:$ckP; $cku"
 rw=$(cat joj | grep -o "Location:.*" | cut -d '/' -f3)
 }
 roj(){
-curl -s "http://$rw/fast_reada/oiejr?jumpid=$ji&code=$jc&state=$js" -H "Host: $rw" -H "Proxy-Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: $ua" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Dest: document" -H "Accept-Encoding: " -H "Cookie: $ck" -H "Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+curl -s "http://$rw/fast_reada/oiejr?jumpid=$ji&code=$jc&state=$js" -H "User-Agent:$ua" -H "Cookie:$ck"
 }
 djs(){
 for ((i=10;i>0;i--))
@@ -118,7 +119,6 @@ read -p "请输入owsq: " owsq
 read -p "请输入ckP: " ckP
 read -p "请输入助力码，不需要助力则回车: " zlm
 owsq
-rk=$(curl -Ls http://m.hg365.top/entry | grep -o "url_h5 =.*" | cut -d '/' -f3 | sed 's/...$//')
 fqrw
 fqfr
 fqzlm
